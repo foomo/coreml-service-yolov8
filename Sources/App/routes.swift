97b19facaddd7a6780a332783fc23698a54d04d6
Jan Halfar <jan@bestbytes.com>
2024-04-29 10:14:34 +0200

    feat: support to read MODELS_PATH env var

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,11 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    let recoController = RecognitionController(modelsPath: "")
+    var modelsPath = ""
+    if Environment.get("MODELS_PATH") != nil {
+        modelsPath = Environment.get("MODELS_PATH")!
+    }
+    let recoController = RecognitionController(modelsPath: modelsPath)
     try app.register(collection: recoController)
     try recoController.loadModel()
 }
